ac9e148a16e4a3b10424a0fe383119ff23983628
lbw <lbw0482@foxmail.com>
2026-09-21 15:45:39 +0800

    feat: 链接数据库暴露orm 为python 包

---
+++ orm/__init__.py
@@ -0,0 +1,4 @@
+from .models import Book
+from .database import get_session,init_db
+
+__init__=["Book","get_session","init_db"]
---
+++ orm/database.py
@@ -0,0 +1,32 @@
+from typing import AsyncGenerator, Any, Generator
+from sqlalchemy.ext.asyncio import(
+  create_async_engine,
+  AsyncSession,
+  async_sessionmaker,
+)
+from sqlalchemy.testing import future
+from sqlmodel import SQLModel
+
+DATABASE_URL="sqlite+aiosqlite:///./orm/books.db"
+
+# 创建异步引擎
+engine =create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True,
+)
+
+# 创建异步异步session工厂
+async_session=async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+# 初始化数据库
+async def init_db()->None:
+    async with engine.begin() as conn:
+        await conn.run_sync((SQLModel.metadata.create_all)
+async def get_session() -> Generator[AsyncSession, None]:
+    async with async_session() as session:
+        yield  session
